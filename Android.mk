d5ab863bd92f75795067271348a10ca27c981fd5
Sam Mortimer <sam@mortimer.me.uk>
2017-10-18 11:44:13 -0700

    external/mksh: guard against overwriting files in system
    *) If a target uses TARGET_COPY_OUT_VENDOR := system
       then writing to TARGET_OUT_VENDOR_EXECUTABLES can
       clobber things in system/bin.
    
    *) Add a guard to prevent this.
    
    Change-Id: Ib9f02e916ac08207e8064a3f9a9a9f4ce03e6cf1

--- Android.mk
+++ Android.mk
@@ -92,6 +92,8 @@ LOCAL_CFLAGS += $(MKSH_CFLAGS)
 
 include $(BUILD_EXECUTABLE)
 
+# Guard to prevent overwriting in system
+ifneq ($(TARGET_COPY_OUT_VENDOR),system)
 # /vendor/etc/mkshrc
 include $(CLEAR_VARS)
 
@@ -125,6 +127,7 @@ LOCAL_CFLAGS += \
 LOCAL_CFLAGS += $(MKSH_CFLAGS)
 
 include $(BUILD_EXECUTABLE)
+endif
 
 MKSH_SRC_FILES:=
 MKSH_CFLAGS:=
